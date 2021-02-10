f4bf8c5ce4c01e68a1516a7555fc243efaa2a74e
Brian Joerger <bjoerger@goteleport.com>
2021-02-10 09:45:07 -0800

    Add DefaultCreds function and credential loaders; Refactor Identity file logic to be in one place.

--- api/client/profile.go
+++ api/client/profile.go
@@ -239,7 +239,7 @@ func (cp *Profile) SaveToDir(dir string, makeCurrent bool) error {
 	if dir == "" {
 		return trace.BadParameter("cannot save profile: missing dir")
 	}
-	if err := cp.SaveToFile(filepath.Join(dir, cp.Name()+".yaml")); err != nil {
+	if err := cp.saveToFile(filepath.Join(dir, cp.Name()+".yaml")); err != nil {
 		return trace.Wrap(err)
 	}
 	if makeCurrent {
@@ -248,8 +248,8 @@ func (cp *Profile) SaveToDir(dir string, makeCurrent bool) error {
 	return nil
 }
 
-// SaveToFile saves this profile to the specified file.
-func (cp *Profile) SaveToFile(filepath string) error {
+// saveToFile saves this profile to the specified file.
+func (cp *Profile) saveToFile(filepath string) error {
 	bytes, err := yaml.Marshal(&cp)
 	if err != nil {
 		return trace.Wrap(err)
